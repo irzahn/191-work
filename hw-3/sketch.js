5420049dfbca42a4ddf7ed337e585a80bfb5a2e2
Isaac Zahn <irzahn80@gmail.com>
2017-02-14 14:36:45 -0700

    finished self portrait.

--- hw-3/sketch.js
+++ hw-3/sketch.js
@@ -2,10 +2,12 @@ function setup() {
 createCanvas(1000,1000);
 background(192,192,192);
 }
+translate(500,500);
 //body
 function draw() {
 fill(25,25,112);
 rect(550,700,400,600,100,100,0,0);
+
 
 //the head
 translate(250,100);
@@ -18,6 +20,9 @@ translate(0, -30);
 fill(0);
 ellipse(410,500,30,20);
 ellipse(540,500,30,20);
+noFill();
+arc(415,500,30,30,TWO_PI,HALF_PI);
+arc(535,500,30,30,HALF_PI,PI);
 pop();
 
 // beard
@@ -30,12 +35,18 @@ pop();
 push();
 fill(0,0,0);
 ellipse(473,590,20,15);
-line(464,585,430,625);
-line(480,585,525,625);
+noFill();
+arc(415,585,100,70,TWO_PI,HALF_PI);
+arc(530,585,100,70,HALF_PI,PI);
+arc(467,595,60,25,QUARTER_PI,PI-HALF_PI);
 pop();
 
-
-
+//arms
+push();
+stroke(200);
+line(630,700,630,1000);
+line(365,700,365,1000);
+pop();
 
 
 }
