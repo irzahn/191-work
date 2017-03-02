8cd074da2a3de13f5a5b20a54e2cbd3a9a35344a
Isaac Zahn <irzahn80@gmail.com>
2017-03-01 21:18:44 -0700

    updated readme

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,7 +1,19 @@
 function setup() {
+createCanvas(600,500);
 
 }
 
 function draw() {
-  
+  background(200);
+  var leftWall = 100;
+  var rightWall = 400;
+  var xm = mouseX;
+  var xc = constrain(mouseX,leftWall,rightWall);
+  Stroke(150);
+  line(100,leftWall,width,leftWall);
+  line(400,rightWall,width,rightWall);
+  Stroke();
+  fill(150);
+  ellipse(xm,33,9,9);
+  ellipse(xc,66,9,9);
 }
