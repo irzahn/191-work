01452c899e3234ef80f3a137fe7467515ebdb032
Isaac Zahn <irzahn80@gmail.com>
2017-03-01 21:08:49 -0700

    finished home work 5 and made read me file

--- hw-5/sketch.js
+++ hw-5/sketch.js
@@ -7,27 +7,60 @@ var col ={
   g:255,
   b:255
   };
-//var leftWall=25;
-//var rightWall=75;
-//var mx=mouseX;
-//var mc=constrain(mouseX,25,75);
 
 
+var mx;
+var mc;
+var star = {
+  size: 1,
+  loc: 0
+};
 function setup() {
-createCanvas(1000,1000);
+createCanvas(500,500);
 background(0);
+
 }
 
 function draw() {
-//line(leftwall,0,leftwall,height);
-//line(rightwall,0,rightwall,height);
+
+
 noStroke();
-fill(col.r,random(0,255),col.b,random(0,255));
+fill(col.r,random(0,255),col.b,random(0,255),col.g,random(0,10),255);
 ellipse(dots.x,dots.y,1,1);
 ellipse(dots.x,dots.y,1,1);
-//ellipse(mx,33,10,10);
-//ellipse(mc,33,10,10);
-dots.x=random(0,width);
-dots.y=random(0,height);
+fill(0,0,random(0,255));
+ellipse(mx,random(180,250),random(1,3),random(1,3),random(0,255));
+ellipse(mc,330,1,1,random(0,255));
+dots.x=constrain(random(0,500),0,width);
+dots.y=constrain(random(0,440),0,height);
+
+
+
+//this is the shooting star
+push();
+noStroke();
+fill(0);
+ellipse(star.loc,150,star.size,star.size);
+star.loc = star.loc +2;//shootingstar
+noStroke();
+fill(255);
+ellipse(star.loc,150,star.size,star.size);
+star.size += 0.05;
+pop();
+fill(25);
+//ellipse(star.loc,150,star.size,star.size,25);
+//body
+fill(0);
+ellipse(250,370,35,35);//head
+rect(230,380,40,70,90,90,0,0);//body
+//tunnel vortex
+translate(width/2,height/2);
+var a = atan2(mouseY-height/2, mouseX-width/2);
+rotate (a);
+fill(25, 0, random (155, 255),15);
+rect(80,50,80,10);
+fill(15,0,random(0,255),15);
+rect(5,50,80,10);
+
 
 }
